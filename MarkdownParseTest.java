d11f4c043299dc7625a96ed27a221978dbb9a4a8
Nathan N <nathansng17@gmail.com>
2022-02-23 19:25:30 -0800

    Added new test command

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -2,6 +2,8 @@
 javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParse.java MarkdownParseTest.java
 
 java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
+
+jdb -classpath "lib/*:." org.junit.runner.JUnitCore MarkdownParseTest
  */
 
 import static org.junit.Assert.*;
