dec5e72368459756cd369ac15698070f598bbf8c
Nathan S Ng <cs15lwi22aoh@ieng6-202.ucsd.edu>
2022-03-02 20:33:03 -0800

    Added new getLinks method to deal with directories

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,22 +1,56 @@
 // File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MarkdownParse {
+
+     public static Map<String, List<String>> getLinks(File dirOrFile) throws IOException {
+        Map<String, List<String>> result = new HashMap<>();
+        if(dirOrFile.isDirectory()) {
+            for(File f: dirOrFile.listFiles()) {
+                result.putAll(getLinks(f));
+            }
+            return result;
+        }
+        else {
+            Path p = dirOrFile.toPath();
+            int lastDot = p.toString().lastIndexOf(".");
+            if(lastDot == -1 || !p.toString().substring(lastDot).equals(".md")) {
+                return result;
+            }
+            ArrayList<String> links = getLinks(Files.readString(p));
+            result.put(dirOrFile.getPath(), links);
+            return result;
+        }
+    }
+  
+
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+	    if (nextOpenBracket < 0) {
+		break;
+	    }
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+	    if (nextCloseBracket < 0) {
+		break;
+	    }
             int openParen = markdown.indexOf("(", nextCloseBracket);
+	    if (openParen < 0) {
+		break;
+	    }
             int closeParen = markdown.indexOf(")", openParen);
-            // Check if no more possible links
-            if (nextOpenBracket < 0) {
-                break;
-            }
+	    if (closeParen < 0) {
+		break;
+	    }
             // Check to make sure it's not an image
             if (nextOpenBracket > 0 && markdown.charAt(nextOpenBracket-1) == '!') {
                 currentIndex = closeParen + 1;
@@ -38,9 +72,10 @@ public class MarkdownParse {
 
     }
     public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
-        ArrayList<String> links = getLinks(contents);
-        System.out.println(links);
+	// Path fileName = Path.of(args[0]);
+	// String contents = Files.readString(fileName);
+        // ArrayList<String> links = getLinks(contents);
+	File file = new File(args[0]);
+        System.out.println(getLinks(file));
     }
 }
